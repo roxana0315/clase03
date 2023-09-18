10b41207442edf003d2f34531244d3873d2adcc5
roxana <2113010120@untel.edu.pe>
2023-09-18 11:03:12 -0500

    ya

---
+++ operadores.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # CUANDO INGRESA UN VALOR POR TECLADO LO HACE COMO CADENA
+
+# In[1]:
+
+
+print ("numero 1: ")
+
+
+# In[2]:
+
+
+numero1=input()
+
+
+# In[3]:
+
+
+print ("numero 2 ")
+
+
+# In[4]:
+
+
+numero2=input()
+
+
+# In[5]:
+
+
+numero3=int(numero1)+int(numero2)
+
+
+# In[6]:
+
+
+print(f"el reultado es {numero3}")
+
+
+# # OPERADORES
+
+# se tiene operadores aritmeticos 
+
+# In[9]:
+
+
+numero1=10
+
+
+# In[8]:
+
+
+numero2=3
+
+
+# In[10]:
+
+
+suma=numero1+numero2
+
+
+# In[11]:
+
+
+reta=numero1-numero2
+
+
+# In[12]:
+
+
+multiplicacion=numero1*numero2
+
+
+# In[13]:
+
+
+division=numero1/numero2
+
+
+# In[14]:
+
+
+residuo=numero1%numero2
+
+
+# In[15]:
+
+
+cociente=numero1//numero2
+
+
+# In[16]:
+
+
+potencia=numero1**numero2
+
+
+# In[20]:
+
+
+suma
+
+
+# In[22]:
+
+
+reta
+
+
+# In[23]:
+
+
+division
+
+
+# In[24]:
+
+
+multiplicacion
+
+
+# In[25]:
+
+
+residuo
+
+
+# In[26]:
+
+
+cociente
+
+
+# In[27]:
+
+
+potencia
+
+
+# In[28]:
+
+
+print(suma)
+
+
+# In[ ]:
+
+
+
+
